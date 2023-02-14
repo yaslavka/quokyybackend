7d93eb8fd85322393d0a2ca867c376bc6d2379f0
yaslavka <obrubinskii@gmail.com>
2023-02-14 15:35:57 +0400

    ok

--- src/controllers/ZakazController/index.js
+++ src/controllers/ZakazController/index.js
@@ -110,7 +110,7 @@ class ZakazController {
     async allZakaz (req, res){
         const zakaz = await Zakaz.findAll()
         console.log('все заказы', zakaz)
-        return res.json(zakaz)
+        return res.json({items:zakaz})
     }
     async vZakaz (req, res){
         const {zakaz} = req.query
