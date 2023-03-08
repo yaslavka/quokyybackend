eca861265f3239ee64477e3e6e7fae417fecfe70
z <ob>
2023-03-08 21:07:17 +0400

    ok

--- src/controllers/UserControllers/index.js
+++ src/controllers/UserControllers/index.js
@@ -95,23 +95,24 @@ class UserController {
     }
     async cengeinfo(req, res, next){
         const { first_name, last_name, phone, email, password, new_password,} = req.body
-        const { authorization } = req.headers;
-        if(!authorization){
-            return res.json('Ненайден айди пользователя');
-        }
-        const token = authorization.slice(7);
-        const decodeToken = jwt.decode(token);
-        const user = await User.findOne({
-            where: { email: decodeToken.email },
-        });
-        let comparePassword = bcrypt.compareSync(password, user.password);
-        if (!comparePassword) {
-            return next(ApiError.internal("Неверный пароль"));
-        }
-        const hashPassword = await bcrypt.hash(new_password, 5);
-        let update = {first_name:first_name, last_name:last_name, phone:phone, email: email, password:hashPassword}
-        await User.update(update, {where:{id:user.id}})
-        return res.json(true)
+        console.log(req.body)
+        // const { authorization } = req.headers;
+        // if(!authorization){
+        //     return res.json('Ненайден айди пользователя');
+        // }
+        // const token = authorization.slice(7);
+        // const decodeToken = jwt.decode(token);
+        // const user = await User.findOne({
+        //     where: { email: decodeToken.email },
+        // });
+        // let comparePassword = bcrypt.compareSync(password, user.password);
+        // if (!comparePassword) {
+        //     return next(ApiError.internal("Неверный пароль"));
+        // }
+        // const hashPassword = await bcrypt.hash(new_password, 5);
+        // let update = {first_name:first_name, last_name:last_name, phone:phone, email: email, password:hashPassword}
+        // await User.update(update, {where:{id:user.id}})
+        // return res.json(true)
 
     }
     async dellete(req, res) {
